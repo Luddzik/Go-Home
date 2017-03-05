5b4c6d230091f909968bacc21e49ef57a5832437
Ludwik Bacmaga <luddzik@gmail.com>
2017-03-05 15:16:47 +0000

    Fixed some bugs
    Fixed some bugs, added build

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-    //[SerializeField]private float startTime;
+
     [SerializeField] private Text currentTime;
     [SerializeField] private Text bestScore;
     [SerializeField] private Text completedLevel;
@@ -14,12 +14,6 @@ public class GameManager : MonoBehaviour {
 
     private float currentScore;
     private float highScore;
-    //private ShowWinScreen win;
-    /*public Rect topScore;
-    public Rect timerRect;
-    public int winScreenWidth, winScreenHeight;
-    public GUISkin skin;
-    private bool showWinScreen = false;*/
 
     private bool ready = true;
     private bool start = false;
@@ -68,24 +62,6 @@ public class GameManager : MonoBehaviour {
         currentTime.text = string.Format("{0:0.00}", currentScore);
     }
 
-    /*void CountDown()
-    {
-        startTime -= Time.deltaTime;
-        currentScore = startTime;
-        currentTime = string.Format("{0:0.00}", startTime);
-        if (startTime <= 0)
-        {
-            startTime = 0;
-            UnityEngine.SceneManagement.SceneManager.LoadScene(8);
-        }
-    }
-
-    public void CompleteLevel()
-    {
-        showWinScreen = true;
-        completed = true;
-    }*/
-
     public void LoadNextLevel()
     {
         if (!UnityEngine.SceneManagement.SceneManager.GetActiveScene().name.Equals("Game Complete"))
@@ -124,35 +100,8 @@ public class GameManager : MonoBehaviour {
     public void WinScreen()
     {
         completed = true;
-        //win.SetActive();
         completedLevel.text = "Completed Level: " + currentLevel.ToString();
         score.text = string.Format("Score: {0:0.00}", currentScore);
     }
-
-    /*
-    void OnGUI()
-    {
-        GUI.skin = skin;
-        GUI.Label(topScore, bestScore, skin.GetStyle("timer"));
-        GUI.Label(timerRect, currentTime, skin.GetStyle("timer"));
-
-        if (showWinScreen)
-        {
-            Rect winScreenRect = new Rect((Screen.width / 2) - (winScreenWidth/2), (Screen.height / 2) - (winScreenHeight /2), winScreenWidth, winScreenHeight);
-            GUI.Box(winScreenRect, "Level Win");
-
-            currentScore = startTime;
-            if (GUI.Button(new Rect(winScreenRect.x + winScreenRect.width - 170, winScreenRect.y + winScreenHeight - 60, 150, 40), "Continue"))
-            {
-                LoadNextLevel();
-            }
-            if (GUI.Button(new Rect(winScreenRect.x + 20, winScreenRect.y + winScreenHeight - 60, 100, 40), "Quit"))
-            {
-                UnityEngine.SceneManagement.SceneManager.LoadScene("main menu");
-            }
-
-            GUI.Label(new Rect(winScreenRect.x + 20, winScreenRect.y + 40, 300, 50), "Completed Level: " + currentLevel.ToString(), skin.GetStyle("timer"));
-            GUI.Label(new Rect(winScreenRect.x + 20, winScreenRect.y + 100, 300, 50), string.Format("Score: {0:0.00}", currentScore), skin.GetStyle("timer"));
-        }
-    }*/
+		
 }
--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour {
     {
         if (PlayerPrefs.GetInt("Level Completed") > 0)
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene(PlayerPrefs.GetInt("Level Complete"));
+            UnityEngine.SceneManagement.SceneManager.LoadScene(PlayerPrefs.GetInt("Level Completed"));
         }
     }
 
@@ -21,40 +21,5 @@ public class MainMenu : MonoBehaviour {
     {
         Application.Quit();
     }
-
-    /*public GUISkin skin;
-
-    void OnGUI()
-    {
-        GUI.skin = skin;
-        GUI.Label(new Rect(Screen.width / 2 -115, Screen.height / 2 - 100, 300, 100), "Go Home");
-        if (PlayerPrefs.GetInt("Level Completed") >0)
-        {
-            if (GUI.Button(new Rect(Screen.width/2 - 50, Screen.height/2 , 100, 50), "Continue"))
-            {
-                UnityEngine.SceneManagement.SceneManager.LoadScene(PlayerPrefs.GetInt("Level Completed"));
-            }
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 60, 100, 50), "New Game"))
-            {
-                PlayerPrefs.SetInt("Level Completed", 0);
-                UnityEngine.SceneManagement.SceneManager.LoadScene(1);
-            }
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 120, 100, 50), "Quit"))
-            {
-                Application.Quit();
-            }
-        }
-        else
-        {
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2, 100, 50), "Play"))
-            {
-                PlayerPrefs.SetInt("Level Completed", 0);
-                UnityEngine.SceneManagement.SceneManager.LoadScene(1);
-            }
-            if (GUI.Button(new Rect(Screen.width / 2 - 50, Screen.height / 2 + 60, 100, 50), "Quit"))
-            {
-                Application.Quit();
-            }
-        }
-    }*/
+		
 }
